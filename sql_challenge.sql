2b6e1e4bde13ce4f264e919e354680e514bef495
aowinfrey1 <88986242+aowinfrey1@users.noreply.github.com>
2022-02-13 00:29:53 -0500

    Add files via upload

--- sql_challenge.sql
+++ sql_challenge.sql
@@ -4,5 +4,15 @@
 --inner join titles on titles.title_id=employees.emp_title_id;
 select first_name, last_name, sex 
 from employees
-where first_name = 'Hercules'
+where first_name = 'Hercules' and last_name >= 'B'
 ORDER BY last_name ASC;
+
+select * from employees
+where hire_date >= '1986-01-01'
+order by hire_date asc;
+
+select 
+count (distinct last_name) as "number of last names"
+from employees
+group by last_name;
+
